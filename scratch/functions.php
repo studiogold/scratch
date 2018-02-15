204e5eb030a2bc2d80f916dceebd990353e8f531
studiogold <36523169+studiogold@users.noreply.github.com>
2018-02-16 10:45:47 +1100

    Add files via upload

---
+++ scratch/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+*Theme customizations
+*
+*@package   Scratch
+*@author    Sharon Dunn
+*@link      http://www.studiogold.com.au
+*@copyright Copyright (c) 2018, Sharon Dunn
+*@license   GPL-2.0+
+*/
+
+load_child_theme_textdomain('scratch');
+
+add_action('genesis_setup', 'scratch_setup');
