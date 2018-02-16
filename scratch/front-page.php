95c58765fb789d503700c4f60c2ef3c3c6f387be
studiogold <36523169+studiogold@users.noreply.github.com>
2018-02-16 13:39:35 +1100

    Add files via upload

--- scratch/front-page.php
+++ scratch/front-page.php
@@ -9,58 +9,65 @@
 * @license   GPL-2.0+
 */
 
-function scratch_home_page() {
-    
-    $home_sidebars = array(
-      'home_welcome' => is_active_sidebar( 'home-welcome'),
-      'call-to-action' => is_active_sidebar( 'call-to-action'), 
-    );
-    
-    // Return early if no sidebars are active.
-    if ( ! in_array(true, $home_sidebars ) ) {
-        return;
-    }
-    
-    // Add home welcome area if 'Home Welcome' widget area is active.
-    if ( $home_sidebars['home_welcome'] ) {
-       add_action( 'genesis_after_header', 'scratch_add_home_welcome' );
-    }
+add_action( 'genesis_meta', 'scratch_home_page_setup' );
+/**
+ * Set up the homepage layout by conditionally loading sections when widgets
+ * are active.
+ *
+ * @since 1.0.0
+ */
+function scratch_home_page_setup() {
 
-    // Add home welcome area if 'Call To Action' widget area is active.
-    if ( $home_sidebars['call_to_action'] ) {
-     add_action( 'genesis_after_header', 'scratch_add_call_to_action' );
-    }
+	$home_sidebars = array(
+		'home_welcome' 	   => is_active_sidebar( 'home-welcome' ),
+		'call_to_action'   => is_active_sidebar( 'call-to-action' ),
+	);
+
+	// Return early if no sidebars are active.
+	if ( ! in_array( true, $home_sidebars ) ) {
+		return;
+	}
+
+	// Add home welcome area if "Home Welcome" widget area is active.
+	if ( $home_sidebars['home_welcome'] ) {
+		add_action( 'genesis_after_header', 'scratch_add_home_welcome' );
+	}
+
+	// Add call to action area if "Call to Action" widget area is active.
+	if ( $home_sidebars['call_to_action'] ) {
+		add_action( 'genesis_after_header', 'scratch_add_call_to_action' );
+	}
 
 }
 
 /**
-* Display content for the "Home Welcome" section.
-*
-* @since 1.0.0
-*/
+ * Display content for the "Home Welcome" section.
+ *
+ * @since 1.0.0
+ */
 function scratch_add_home_welcome() {
-    
-    genesis_widget_area( 'home-welcome',
-        array(
-           'before' => '<div class="home-welcome"><div class="wrap">',
-        )               
-    );
-    
+
+	genesis_widget_area( 'home-welcome',
+		array(
+			'before' => '<div class="home-welcome"><div class="wrap">',
+			'after' => '</div></div>',
+		)
+	);
 }
 
 /**
-* Display content for the "Call to Action" section.
-*
-* @since 1.0.0
-*/
+ * Display content for the "Call to Action" section.
+ *
+ * @since 1.0.0
+ */
 function scratch_add_call_to_action() {
-    
-    genesis_widget_area( 'call-to-action',
-        array(
-           'before' => '<div class="call-to-action"><div class="wrap">',
-        )               
-    );
-    
+
+	genesis_widget_area( 'call-to-action',
+		array(
+			'before' => '<div class="call-to-action"><div class="wrap">',
+			'after' => '</div></div>',
+		)
+	);
 }
 
 genesis();
--- scratch/includes/widget-areas.php
+++ scratch/includes/widget-areas.php
@@ -10,15 +10,14 @@
 */
  
  
-//* Register front page widget area
-    genesis_register_sidebar( array(
+// Register front page widget areas
+genesis_register_sidebar( array(
 	'id'            => 'home-welcome',
 	'name'          => __( 'Home Welcome', 'scratch' ),
 	'description'   => __( 'This is a home widget area that will show on the front page', 'scratch' ),
 ) );
-    
-    genesis_register_sidebar( array(
+genesis_register_sidebar( array(
 	'id'            => 'call-to-action',
-	'name'          => __( 'Call To Action', 'scratch' ),
+	'name'          => __( 'Call to Action', 'scratch' ),
 	'description'   => __( 'This is a call to action widget area that will show on the front page', 'scratch' ),
 ) );
