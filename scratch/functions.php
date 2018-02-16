4637aff6f6a6a00a4dcd34aeb21f509129f4764b
studiogold <36523169+studiogold@users.noreply.github.com>
2018-02-16 11:50:54 +1100

    Add files via upload

--- scratch/functions.php
+++ scratch/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
 * Theme customizations
 *
@@ -9,10 +8,8 @@
 * @copyright Copyright (c) 2018, Sharon Dunn
 * @license   GPL-2.0+
 */
-
 // Load child theme textdomain.
 load_child_theme_textdomain('scratch');
-
 add_action('genesis_setup', 'scratch_setup');
 /**
 * Theme setup.
@@ -21,8 +18,39 @@ add_action('genesis_setup', 'scratch_setup');
 *
 * @since 1.0.0
 */
-
-
 function scratch_setup() {
+    
+    // Define theme constants.
+    define( 'CHILD_THEME_NAME', 'Scratch' );
+    define( 'CHILD_THEME_URL', 'http://github.com/studiogold/scratch' );
+    define( 'CHILD_THEME_VERSION', '1.0.0' );
+    
+    // Add HTML5 markup structure.
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+    
+    // Add viewport meta tag for mobile browsers.
+    add_theme_support( 'genesis-responsive-viewport' );
+    
+    // Add theme support for accessibility.
+    add_theme_support( 'genesis-accessibility', array(
+            '404-page',
+            'drop-down-menu',
+            'headings',
+            'rems',
+            'search-form',
+            'skip-links',
+    
+    ) );
+    
+    // Add them support for footer widgets.
+    add_theme_support( 'genesis-footer-widgets', 3);
+    
+   // Unregister layouts that use secondary sidebar.
+    genesis_unregister_layout( 'content-sidebar-sidebar' );
+    genesis_unregister_layout( 'sidebar-content-sidebar' );
+    genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+    
+    
     
 }
