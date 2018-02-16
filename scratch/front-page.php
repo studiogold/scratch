517ab0963584de0b3dc7c17bdf2e905b1548ce67
studiogold <36523169+studiogold@users.noreply.github.com>
2018-02-16 11:11:37 +1100

    Add files via upload

---
+++ scratch/front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Front page template
+*
+* @package   Scratch
+* @author    Sharon Dunn
+* @link      http://www.studiogold.com.au
+* @copyright Copyright (c) 2018, Sharon Dunn
+* @license   GPL-2.0+
+*/
+
+genesis();
--- scratch/functions.php
+++ scratch/functions.php
@@ -1,16 +1,28 @@
 <?php
 
 /**
-*Theme customizations
+* Theme customizations
 *
-*@package   Scratch
-*@author    Sharon Dunn
-*@link      http://www.studiogold.com.au
-*@copyright Copyright (c) 2018, Sharon Dunn
-*@license   GPL-2.0+
+* @package   Scratch
+* @author    Sharon Dunn
+* @link      http://www.studiogold.com.au
+* @copyright Copyright (c) 2018, Sharon Dunn
+* @license   GPL-2.0+
 */
 
 // Load child theme textdomain.
 load_child_theme_textdomain('scratch');
 
 add_action('genesis_setup', 'scratch_setup');
+/**
+* Theme setup.
+*
+* Attach all of the site-wide functions to the correct hooks and filters. All the functions themselves are defined below this setup function.
+*
+* @since 1.0.0
+*/
+
+
+function scratch_setup() {
+    
+}
